fddc5eae2e7015127e9c04f08ddc0abb83b7ff1b
Eugene Tolbakov <ev.tolbakov@gmail.com>
2023-06-30 21:08:52 +0100

    fix: adjust setting for the config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ -n "$INPUT_PATH" ]; then
   exec excalidocker --input-path "$INPUT_PATH" ${SKIP_DEPS:+ --skip-dependencies} --config-path ${CONFIG_PATH:-/excalidocker/bin/excalidocker-config.yaml}
 elif [ -n "$SHOW_CONFIG" ]; then
-  exec excalidocker -C
+  exec excalidocker -C --config-path /excalidocker/bin/excalidocker-config.yaml
 else
   exec excalidocker -h
 fi
